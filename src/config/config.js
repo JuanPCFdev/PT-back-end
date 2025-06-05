a116ef487a962071f6380347c391cd73fc60045a
Juan Cuellar <144962816+JuanPCFdev@users.noreply.github.com>
2025-06-05 15:47:27 -0500

    amazing location
    adding new amazing location

--- src/config/config.js
+++ src/config/config.js
@@ -3,8 +3,8 @@ const config = {
     port: process.env.PORT || 3000,
     // Coordenadas iniciales del mapa (Bogotá, Colombia)
     defaultMapCoordinates: {
-        lat: 4.60971,
-        lng: -74.08175
+        lat: 4.664685054155181,
+        lng: -74.05427247562909
     },
     // Usuario de prueba
     testUser: {
